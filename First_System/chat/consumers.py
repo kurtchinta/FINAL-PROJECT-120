90e0d701616ada535ac30ccb183e0df43212a25a
Johndave Lausa <johndavelausa@gmail.com>
2024-12-18 23:56:50 -0800

    Added Fernet Encryption Middleware

--- First_System/chat/consumers.py
+++ First_System/chat/consumers.py
@@ -3,6 +3,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from .models import Message
 from django.contrib.auth.models import User
+from .middleware import EncryptionMiddleware
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -30,28 +31,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
         room = data['room']
 
 
-        await self.save_message(username, room, message)
+        encrypted_message = EncryptionMiddleware.encrypt(message)
+
+        await self.save_message(username, room, encrypted_message)
         # Broadcast the message to the group
         await self.channel_layer.group_send(
             self.room_group_name, {
                 'type': 'chat_message',
-                'message': message,
+                'message': encrypted_message,
                 'username': username
             }
         )
 
     async def chat_message(self, event):
-        message = event['message']
+        encrypted_message = event['message']
         username = event['username']
+
+        decrypted_message = EncryptionMiddleware.decrypt(encrypted_message)
 
         # Send the message to WebSocket clients
         await self.send(text_data=json.dumps({
-            'message': message,
+            'message': decrypted_message,
             'username': username
         }))
 
     @sync_to_async
-    def save_message(self, username, room, message):
+    def save_message(self, username, room, encrypted_message):
         user = User.objects.get(username=username)
     # Create the message using the user instance
-        Message.objects.create(user=user, username=username, room=room, content=message)
+        Message.objects.create(user=user, username=username, room=room, content=encrypted_message)
